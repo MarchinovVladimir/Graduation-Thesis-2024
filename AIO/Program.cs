f6fb0499582a2809e8e5e3cff1746473580cff1a
MarchinovVladimir <114493729+MarchinovVladimir@users.noreply.github.com>
2024-03-06 17:42:10 +0200

    Anti-forgery token added

--- AIO/Program.cs
+++ AIO/Program.cs
@@ -2,6 +2,7 @@ using AIO.Data;
 using AIO.Data.Models;
 using AIO.Services.Data.Interfaces;
 using AIO.Web.Infrastructure.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -10,31 +11,35 @@ builder.Services.AddApplicationDbContext(builder.Configuration);
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
-    options.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
-    options.Password.RequireDigit = builder.Configuration.GetValue<bool>("Identity:Password:RequireDigit");
-    options.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
-    options.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
-    options.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
-    options.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
+	options.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+	options.Password.RequireDigit = builder.Configuration.GetValue<bool>("Identity:Password:RequireDigit");
+	options.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+	options.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
+	options.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+	options.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
 })
 .AddEntityFrameworkStores<AIODbContext>();
 
 builder.Services.AddApplicationServices(typeof(IProductService));
 
-builder.Services.AddControllersWithViews(option => option.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider()));
+builder.Services.AddControllersWithViews(option =>
+{
+	option.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+	option.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+});
 
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
-    app.UseDeveloperExceptionPage();
+	app.UseMigrationsEndPoint();
+	app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error/500");
-    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error/500");
+	app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+	app.UseHsts();
 }
 
 
@@ -46,7 +51,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapDefaultControllerRoute();    
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 await app.RunAsync();
