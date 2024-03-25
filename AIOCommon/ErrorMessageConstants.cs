1ef969bbcb87f60ad07e2035511f41dff65eb605
MarchinovVladimir <114493729+MarchinovVladimir@users.noreply.github.com>
2024-03-25 18:10:27 +0200

    Product Add() action refactored

--- AIOCommon/ErrorMessageConstants.cs
+++ AIOCommon/ErrorMessageConstants.cs
@@ -17,6 +17,8 @@ namespace AIOCommon
             public const string URLInvalidErrorMessage = "Invalid URL.";
             public const string PriceRangeErrorMessage = "{0} must be between {2} and {1}";
             public const string GeneralErrorMessage = "Unexpected error occured. Please try again later or contact administrator!";
+            public const string CategoryNotExistsErrorMessage = "Selected category does not exist";
+            public const string UnsuccesfulProductAddErrorMessage = "An error occurred while adding the product. Please try again later or contact administrator!";
 		}
     }
 }
