763c37a8d17a2ae8550e36297d150752fc4fb790
MarchinovVladimir <114493729+MarchinovVladimir@users.noreply.github.com>
2024-04-17 13:09:56 +0300

    ClaimsPrincipalsExtensionsSummary added

--- AIO.Web.Infrastructure/Extentions/ClaimsPrincipalExtentions.cs
+++ AIO.Web.Infrastructure/Extentions/ClaimsPrincipalExtentions.cs
@@ -3,29 +3,29 @@ using static AIOCommon.GeneralAppConstants;
 
 namespace AIO.Web.Infrastructure.Extentions
 {
-    /// <summary>
-    /// Extention class for ClaimsPrincipal.
-    /// </summary>
-    public static class ClaimsPrincipalExtentions
-    {
-        /// <summary>
-        /// Extention method for getting user id from ClaimsPrincipal.
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        public static string GetId(this ClaimsPrincipal user)
-        {
-            return user.FindFirstValue(ClaimTypes.NameIdentifier);
-        }
+	/// <summary>
+	/// Extention class for ClaimsPrincipal.
+	/// </summary>
+	public static class ClaimsPrincipalExtentions
+	{
+		/// <summary>
+		/// Extention method for getting user id from ClaimsPrincipal.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public static string GetId(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
 
-        /// <summary>
-        /// Extention method for checking is the user in admin role from ClaimsPrincipal.
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        public static bool IsAdmin(this ClaimsPrincipal user)
-        {
+		/// <summary>
+		/// Extention method for checking is the user in admin role from ClaimsPrincipal.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public static bool IsAdmin(this ClaimsPrincipal user)
+		{
 			return user.IsInRole(AdminRoleName);
 		}
-    }
+	}
 }
