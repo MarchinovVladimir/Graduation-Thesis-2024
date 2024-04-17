79e3e32045c8a5e350f477a34f51fb7422bbdc88
MarchinovVladimir <114493729+MarchinovVladimir@users.noreply.github.com>
2024-04-17 12:47:27 +0300

    Summeries added to ISellerService

--- AIO.Services.Data/Interfaces/ISellerService.cs
+++ AIO.Services.Data/Interfaces/ISellerService.cs
@@ -2,16 +2,46 @@
 
 namespace AIO.Services.Data.Interfaces
 {
+    /// <summary>
+    /// Interface for SellerService
+    /// </summary>
     public interface ISellerService
     {
+        /// <summary>
+        /// Check if seller exist by user id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         Task<bool> IsSellerExistByUserIdAsync(string userId);
 
+        /// <summary>
+        /// Check if seller exist by phone number
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns></returns>
         Task<bool> IsSellerExistByPhoneNumberAsync(string phoneNumber);
 
+        /// <summary>
+        /// Create new seller
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         Task CreateAsync(string userId, BecomeSellerFormModel model);
 
+        /// <summary>
+        /// Get seller id by user id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         Task<string> GetSellerIdByUserIdAsync(string userId);
 
+        /// <summary>
+        /// Check if seller has product with id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="productId"></param>
+        /// <returns></returns>
         Task<bool> HasProductWithIdAsync(string userId, string productId);
     }
 }
